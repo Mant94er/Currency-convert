7ea1ca566c3c86e1d52c7d13826045fdc0d1f297
Mant94er <mant94er@gmail.com>
2024-05-05 19:42:36 +0300

    improvements

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 #currency-conversion project
-My first attempt at creating a project with Vue.js. It is a currency-conversion app
+My first attempt at creating a project with Vue.js. It is a currency-conversion page
--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,39 @@
 <template>
   <div id="app">
     <h1>💸Currency converter by Manos T.💸</h1>
-    <p id="form">
-      Fill in the sum you wish to convert:
-      <input id="conversion" type="text" @change="setSum" />
-      <!-- <button @click="confirmInput">Enter</button> -->
-    </p>
-    <p>
-      Choose current currency:
-      <select
-        name="currency-before"
-        id="currency-before"
-        @change="setCurrentCurrency"
-      >
-        <option value="">-Select-</option>
-        <option v-for="(coin, i) in coins" :key="i">{{ coin.name }}</option>
-      </select>
-    </p>
-    <p>
-      Choose currency to convert to:
-      <select @change="setFinalCurrency" name="currency" id="currency-after">
-        <option value="">-Select-</option>
-        <option v-for="(coin, i) in coins" :key="i">{{ coin.name }}</option>
-      </select>
-    </p>
-    <p><button @click="convert">Convert</button></p>
+    <form @submit.prevent="convert">
+      <div class="sum">
+        <p>
+          Fill in the sum you wish to convert:
+          <input id="conversion" type="number" v-model="confirmedValue" />
+        </p>
+      </div>
+      <div>
+        <p>
+          Choose current currency:
+          <select
+            name="currency-before"
+            id="currency-before"
+            v-model="currentCurrency"
+          >
+            <option value="init">-Select-</option>
+            <option v-for="(coin, i) in coins" :key="i">{{ coin.name }}</option>
+          </select>
+        </p>
+      </div>
+      <div>
+        <p>
+          Choose currency to convert to:
+          <select v-model="finalCurrency" name="currency" id="currency-after">
+            <option value="init">-Select-</option>
+            <option v-for="(coin, i) in coins" :key="i">{{ coin.name }}</option>
+          </select>
+        </p>
+      </div>
+      <div>
+        <p><button>Convert</button></p>
+      </div>
+    </form>
     <h3>
       <p>Outcome of conversion: {{ result }}</p>
     </h3>
@@ -34,7 +43,7 @@
 export default {
   data() {
     return {
-      confirmedValue: 0,
+      confirmedValue: null,
       coins: [
         { name: 'EUR', exchangeToEUR: 0, exchangeFromEUR: 0 },
         { name: 'USD', exchangeToEUR: 0, exchangeFromEUR: 0 },
@@ -42,8 +51,8 @@ export default {
         { name: 'TRY', exchangeToEUR: 0, exchangeFromEUR: 0 },
         { name: 'GBP', exchangeToEUR: 0, exchangeFromEUR: 0 },
       ],
-      currentCurrency: 'EUR',
-      finalCurrency: '',
+      currentCurrency: 'init',
+      finalCurrency: 'init',
       conversionTo: 0,
       conversionFrom: 0,
       result: 0,
@@ -63,31 +72,45 @@ export default {
           });
         });
     },
-    setSum(e) {
-      e.preventDefault();
-      this.confirmedValue = e.target.value;
-    },
-    setCurrentCurrency(e) {
-      e.preventDefault();
-      const coined = this.coins.find((coin) => coin.name === e.target.value);
-      this.currentCurrency = coined.name;
-      this.conversionTo = coined.exchangeToEUR;
-    },
-    setFinalCurrency(e) {
-      e.preventDefault();
-      const coined = this.coins.find((coin) => coin.name === e.target.value);
-      this.finalCurrency = coined.name;
-      this.conversionFrom = coined.exchangeFromEUR;
-    },
+    // setSum(e) {
+    //   e.preventDefault();
+    //   this.confirmedValue = e.target.value;
+    //   if (!e.target.value) console.log('Well done!');
+    // },
+    // setCurrentCurrency(e) {
+    //   e.preventDefault();
+    //   const coined = this.coins.find((coin) => coin.name === e.target.value);
+    //   this.currentCurrency = coined.name;
+    //   this.conversionTo = coined.exchangeToEUR;
+    // },
+    // setFinalCurrency(e) {
+    //   e.preventDefault();
+    //   const coined = this.coins.find((coin) => coin.name === e.target.value);
+    //   this.finalCurrency = coined.name;
+    //   this.conversionFrom = coined.exchangeFromEUR;
+    // },
     convert() {
+      const coin1 = this.coins.find(
+        (coin) => coin.name === this.currentCurrency
+      );
+      this.conversionTo = coin1.exchangeToEUR;
+      const coin2 = this.coins.find((coin) => coin.name === this.finalCurrency);
+      this.conversionFrom = coin2.exchangeFromEUR;
+      if (!this.confirmedValue) {
+        this.result = '';
+        return;
+      }
       this.result = (
         this.confirmedValue *
         (this.conversionTo * this.conversionFrom)
       ).toFixed(2);
-      console.log(this.conversionTo, this.conversionFrom);
       this.result
         ? this.result
         : alert('The sum you have entered is not valid! Try again');
+      setTimeout(() => {
+        this.currentCurrency = this.finalCurrency = 'init';
+        this.confirmedValue = null;
+      }, 2000);
     },
   },
   mounted: function () {
